31a38dbf08dfa7eec7a7e2465f0b408d77b14306
Abhrajit Debnath <137435505+Abhrajit-debnath@users.noreply.github.com>
2024-10-10 10:04:00 +0530

    fix typo

--- bin/postinstall.js
+++ bin/postinstall.js
@@ -1,13 +1,12 @@
-const chalk = require("chalk");
-console.log(chalk.blue("Welcome to Note-Toolkit"));
-console.log(
-  chalk.green(`
+const chalk = require('chalk');
+
+console.log(chalk.green(`
     _   _       _     _     _ _    _ _    
-   | \\\\ | |     | |   | |   (_) |  (_) |   
-   |  \\\\| |_   _| |_  | |    _| | ___| |_  
-   | . \\\` | | | | __| | |   | | |/ _ \\\\ __| 
-   | |\\\\  | |_| | |_  | |___| | |  __/ |_  
-   |_| \\\\_|\\\\__,_|\\\\__| \\\\_____/_|_|\\\\___|\\\\__| 
-                                          
-`)
-);
+   | \\ | |     | |   | |   (_) |  (_) |   
+   |  \\| |_   _| |_  | |    _| | ___| |_  
+   | . \` | | | | __| | |   | | |/ _ \\ __| 
+   | |\\  | |_| | |_  | |___| | |  __/ |_  
+   |_| \\_|\\__,_|\\__| \\_____/_|_|\\___|\\__| 
+                                           
+Note Toolkit has been successfully installed!
+`));
